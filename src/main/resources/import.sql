e9364c43172e5d4b6e2b2031ce9250bce43451b4
Andre Lomaka <andre7lomaka@gmail.com>
2021-07-04 21:07:58 +0300

    import.sql modification

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -34,6 +34,6 @@ VALUES('RO'),
       ('AI');
 
 INSERT INTO trip(from_city_id, from_airport_id, to_city_id, to_hotel_id, to_airport_id, departure_date, return_date, board_basis_type_id, adult_price, child_price, promoted, available, number_of_adult_beds, number_of_child_beds)
-VALUES(1, 1, 2, 2, 2, '2021-07-04', '2020-07-05', 1, 3.0, 1.5, false, true, 1, 1),
-      (2, 2, 1, 1, 1, '2021-07-05', '2020-07-06', 2, 6.05, 7.0, false, true, 2, 3),
-      (1, 1, 2, 2, 2, '2021-07-27', '2020-07-08', 3, 8.0, 9.0, false, true, 1, 0);
+VALUES(1, 1, 2, 2, 2, '2021-07-04', '2021-07-05', 1, 3.0, 1.5, false, true, 1, 1),
+      (2, 2, 1, 1, 1, '2021-07-05', '2021-07-06', 2, 6.05, 7.0, false, true, 2, 3),
+      (1, 1, 2, 2, 2, '2021-07-27', '2021-07-08', 3, 8.0, 9.0, false, true, 1, 0);
