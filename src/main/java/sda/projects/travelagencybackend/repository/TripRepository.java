a1a091b3a47b2889637cb125b95e20d205498579
Andre Lomaka <andre7lomaka@gmail.com>
2021-07-07 13:48:04 +0300

    Changed queries from native to JPQL

--- src/main/java/sda/projects/travelagencybackend/repository/TripRepository.java
+++ src/main/java/sda/projects/travelagencybackend/repository/TripRepository.java
@@ -12,19 +12,18 @@ public interface TripRepository extends JpaRepository<Trip, Long> {
     List<Trip> findAllByDepartureDateGreaterThan(LocalDate localDate);
     List<Trip> findAllByPromotedAndDepartureDateGreaterThan(boolean promoted, LocalDate localDate);
 
-    @Query(value = "SELECT * FROM trip WHERE to_city_id IN (SELECT id FROM city WHERE country_id IN (SELECT id FROM country WHERE continent_id = :id)) AND departure_date > :date", nativeQuery = true)
+    @Query(value = "SELECT t FROM Trip t WHERE t.toCity.id IN (SELECT ci.id FROM City ci WHERE ci.country.id IN (SELECT co.id FROM Country co WHERE co.continent.id = :id)) AND t.departureDate > :date")
     List<Trip> findAllByContinent(@Param("id") Long id, @Param("date") LocalDate date);
 
-    @Query(value = "SELECT * FROM trip WHERE to_city_id IN (SELECT id FROM city WHERE country_id = :id) AND departure_date > :date", nativeQuery = true)
+    @Query(value = "SELECT t FROM Trip t WHERE t.toCity.id IN (SELECT ci.id FROM City ci WHERE ci.country.id = :id) AND t.departureDate > :date")
     List<Trip> findAllByCountry(@Param("id") Long id, @Param("date") LocalDate date);
 
-    @Query(value = "SELECT * FROM trip WHERE from_city_id = :id AND departure_date > :date", nativeQuery = true)
+    @Query(value = "SELECT t FROM Trip t WHERE t.fromCity.id = :id AND t.departureDate > :date")
     List<Trip> findAllByFromCity(@Param("id") Long id, @Param("date") LocalDate date);
 
-    @Query(value = "SELECT * FROM trip WHERE to_city_id = :id AND departure_date > :date", nativeQuery = true)
+    @Query(value = "SELECT t FROM Trip t WHERE t.toCity.id = :id AND t.departureDate > :date")
     List<Trip> findAllByToCity(@Param("id") Long id, @Param("date") LocalDate date);
 
-//    @Query(value = "SELECT * FROM trip WHERE board_basis_type_id = :id AND departure_date > :date", nativeQuery = true)
     @Query(value = "SELECT t FROM Trip t WHERE t.boardBasisType.id = :id AND t.departureDate > :date")
     List<Trip> findAllByBbt(@Param("id") Long id, @Param("date") LocalDate date);
 };
