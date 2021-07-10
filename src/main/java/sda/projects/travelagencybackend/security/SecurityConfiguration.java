f859f69e742443900be8601fa0ce4bceff7416a8
Andre Lomaka <andre7lomaka@gmail.com>
2021-07-10 19:29:31 +0300

    Added SecurityConfigurator

---
+++ src/main/java/sda/projects/travelagencybackend/security/SecurityConfiguration.java
@@ -0,0 +1,33 @@
+package sda.projects.travelagencybackend.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+   private static final String password = "{bcrypt}$2y$12$fqITdfOgS2Z3QnL781Yf9OcmlPi3O5TwVAUnGqS2EDYiXI6zDsxyu";
+
+   @Override
+   protected void configure(final HttpSecurity hs) throws Exception {
+      hs.authorizeRequests().
+           antMatchers(HttpMethod.POST, "/api/trips/**").hasRole("ADMIN").
+           antMatchers(HttpMethod.DELETE, "/api/trips/**").hasRole("ADMIN").
+//           antMatchers(HttpMethod.GET, "/api/trips/**").hasRole("ADMIN").
+           anyRequest().permitAll().
+           and().httpBasic().
+//           and().formLogin().
+           and().logout().logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.OK)).
+           and().csrf().disable();
+   }
+
+   @Override
+   protected void configure(final AuthenticationManagerBuilder amb) throws Exception {
+      amb.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN");
+   }
+}
